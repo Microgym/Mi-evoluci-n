f220f6141a3f9b302f26f29206f4a5f5185c242d
Microgym <miguel.ferrer@rinfresco.es>
2026-09-10 16:10:21 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='mi-evolucion-v22-1-8';
+const CACHE='mi-evolucion-v22-1-12';
 const ASSETS=['./','./index.html','./manifest.webmanifest'];
 self.addEventListener('install',e=>{
   self.skipWaiting();
