27bf536c3596d761c1251f3be5a0d5e6d0826239
Microgym <miguel.ferrer@rinfresco.es>
2026-09-23 05:50:09 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='micro-health-v22-2-36';
+const CACHE='micro-health-v22-2-37';
 const CORE=['./','./index.html','./manifest.webmanifest','./apple-touch-icon.png','./icon-192.png','./icon-512.png','./favicon-32.png'];
 
 self.addEventListener('install',event=>{
